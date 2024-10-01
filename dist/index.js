0a8037d26ccda1c76b2c4eb396e3bb5d62a65a06
itamar1996 <itay9919@gmail.com>
2024-10-01 12:30:38 +0300

    create data and models(player game)

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+require("dotenv/config");
+const app = (0, express_1.default)();
+app.use(express_1.default.json()); // to get the req.body
+app.use((0, cookie_parser_1.default)()); // to get the req.cookie(s)
+app.listen(process.env.port, () => console.log(`Server is up and running, feel free to visit at http://localhost:${process.env.PORT}`));
